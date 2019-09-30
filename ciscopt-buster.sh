463de5ae400d788de66b19dfab63b4d1f56f2b67
Brunopvh <43321452+Brunopvh@users.noreply.github.com>
2019-09-30 11:19:13 -0400

    Update ciscopt-buster.sh

--- ciscopt-buster.sh
+++ ciscopt-buster.sh
@@ -17,7 +17,6 @@
 
 clear
 
-
 # Cores
 amarelo="\e[1;33m"
 Amarelo="\e[1;33;5m"
@@ -29,38 +28,47 @@ fecha="\e[m"
 
 esp="------------------------------------------"
 
-# Detectar sistema.
-codinome_sistema=$(grep '^VERSION_CODENAME' /etc/os-release | sed 's|.*=||g')
-nome_sistema=$(grep '^ID=' /etc/os-release | sed 's|.*=||g')
+#----------------------[ DETECTAR SISTEMA E CODNOME ]----------------------#
+export codinome_sistema=$(grep '^VERSION_CODENAME' /etc/os-release | sed 's|.*=||g')
+export nome_sistema=$(grep '^ID=' /etc/os-release | sed 's|.*=||g')
 
-[[ $codinome_sistema == buster ]] || { echo -e "${vermelho}Seu sistema não e Debian buster $fecha"; exit 1; }
+if [[ "$codinome_sistema" != "buster" ]] && [[ "$codinome_sistema" != "bionic" ]]; then
 
+	# Se for linuxmint tina, tratar como ubuntu bionic.
+    if [[ $nome_sistema == 'linuxmint' ]] && [[ $codinome_sistema == 'tina' ]]; then
+        export nome_sistema="ubuntu"
+        export codinome_sistema="bionic"
+
+    else
+
+	    printf "${vermelho}Este programa e incompativel com seu sistema ${fecha}\n"
+	    exit 1
+    fi
+fi
+
+#------------------ Detectar se usuário e root ---------#
 if [[ $USER == root ]] || [[ $UID == 0 ]]; then 
 	echo -e "${vermelho}Usuário não pode ser o [root] $fecha"
 	exit 1
 fi
 
 
+# Diretórios
+DIR_APPS_LINUX="${HOME}"/"${codinome_sistema}"
+dir_packettracer_tmp="$DIR_APPS_LINUX"/tmp
+dir_libs="$DIR_APPS_LINUX"/lib-png
+
 # Links
 ftp_us="http://ftp.us.debian.org/debian/pool/main"
 security="http://security.debian.org/debian-security"
 
 link_libpng12_deb8_amd64="${ftp_us}/libp/libpng/libpng12-0_1.2.50-2+deb8u3_amd64.deb"
-link_libpng12_deb8_i386="${ftp_us}/libp/libpng/libpng12-0_1.2.50-2+deb8u3_i386.deb"
 link_libssl1_deb8_amd64="${security}/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u11_amd64.deb"
-link_libssl1_deb8_i386="${security}/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u11_i386.deb"
 
-# Diretórios
-DIR_APPS_LINUX="${HOME}"/"${codinome_sistema}"
-DIR_INTERNET="${DIR_APPS_LINUX}/Internet"
-dir_tmp_pt="${DIR_INTERNET}/cisco-pt72-amd64"
-dir_libs="${HOME}/.local/tmp/libs"
+arq_libssl1_deb8_amd64="${DIR_APPS_LINUX}"/libssl1.0_deb8_amd64.deb
+arq_libpng12_deb8_amd64="${DIR_APPS_LINUX}"/libpng12-0_deb8_amd64.deb
 
-arq_libssl1_deb8_amd64="${DIR_INTERNET}"/libssl1.0_deb8_amd64.deb
-arq_libssl1_deb8_i386="${DIR_INTERNET}"/libssl1.0_deb8_i386.deb
-arq_libpng12_deb8_amd64="${DIR_INTERNET}"/libpng12-0_deb8_amd64.deb
-
-mkdir -p "$DIR_APPS_LINUX" "$DIR_INTERNET" "$dir_tmp_pt" "$dir_libs"
+mkdir -p "$DIR_APPS_LINUX" "$dir_packettracer_tmp" "$dir_libs"
 
 # export local_trab=$(dirname $0) 
 export readonly local_trab=$(dirname $(readlink -f "$0")) # Path do programa no sistema.
@@ -69,6 +77,13 @@ export readonly local_trab=$(dirname $(readlink -f "$0")) # Path do programa no 
 	echo -e "Você não tem permissão de escrita em: $local_trab"
 	exit
 }
+
+
+#------------ Função para exibir mensagens ---------------#
+function msgs() {
+	echo -e "$1" "$2 ${fecha}"
+}
+
 
 # Função para verificar ou instalar os programas necessários.
 function _apps_exist() 
@@ -118,7 +133,7 @@ esac
 
 }
 
-#---------------------------[ Instalar cisco pt ]-----------#
+#-------------------[ Instalar cisco packettracer pacote .run/.tar.gz ]-----------#
 function _inst_ciscopkt() 
 {
 
@@ -131,33 +146,51 @@ while true; do
 
 	[[ -z "$arq_instalacao" ]] && exit 1
 
+	# Arquivo termina com .tar.gz ou .run ?
+	[[ $(echo "$arq_instalacao" | egrep "(.tar.gz$|.run$)") ]] || { msgs "$vermelho" "Arquivo inválido"; exit 1; }
+
 	prosseguir=$(msgs_zenity "--list" "Usar este arquivo ?" "Continuar" "650" "250" "$nome_arq" "Sim Não")
 	
-	# Descompactar e instalar packettracer.
+	# Descompactar e instalar.
 	if [[ "$prosseguir" == "Sim" ]] && [[ $(echo "$nome_arq" | egrep ".tar.gz$" ) ]]; then
-		echo 'Instalando .tar.gz'
-	
-		sudo rm -rf "${dir_tmp_pt}"/* 1> /dev/null 2>&1s
-		tar xvzf "$arq_instalacao" -C "$dir_tmp_pt"
-		chmod +x "$dir_tmp_pt/install"
-		cd "$dir_tmp_pt/" && ./install
+		echo -e "Instalando: $arq_instalacao"		
+
+		# Limpar o diretório onde o arquivo será extraido.
+		sudo rm -rf "${dir_packettracer_tmp}"/* 1> /dev/null 2>&1 
+
+		echo "Descompactando"
+		[[ $(tar xvzf "$arq_instalacao" -C "$dir_packettracer_tmp") ]] || { 
+			msgs "$vermelho" "Falha ao tentar descompactar" ; exit 1; 
+		}
+
+		chmod +x "$dir_packettracer_tmp"/install
+		sed -i 's|^more .*||g' "$dir_packettracer_tmp"/install
+
+		cd "$dir_packettracer_tmp" && ./install
 		(_corrigir_arquivos)
+
 		break
+
 	elif [[ "$prosseguir" == "Sim" ]] && [[ $(echo "$nome_arq" | egrep ".run$" ) ]]; then
+
 		echo 'Instalando .run'
 		chmod +x "$arq_instalacao"
 		"$arq_instalacao"
-		#(_corrigir_arquivos)
+		(_corrigir_arquivos)
+
 		break
 
 	elif [[ "$prosseguir" == "Não" ]]; then
 		echo 'Não...'
 		exit 1
 		break 
+
 	else
+
 		echo 'Repetindo...'
 		continue
 	fi
+
 done
 } # Fim _inst_ciscopkt
 
@@ -174,11 +207,15 @@ function _inst_libs_buster() {
 	# libpng12 deb8 amd64
 	[[ -f '/opt/pt/bin/libpng12.so.0.50.0' ]] || {
 		echo 'Configurando libpng12'
+
 		wget "$link_libpng12_deb8_amd64" -O "$arq_libpng12_deb8_amd64"
 		sudo rm -rf "$dir_libs"/* 1> /dev/null 2>&1
 		sudo dpkg-deb -x "$arq_libpng12_deb8_amd64" "$dir_libs"
 		sudo cp -vu "${dir_libs}"/lib/x86_64-linux-gnu/libpng12.so.0.50.0 /opt/pt/bin/libpng12.so.0.50.0
-		sudo ln -sf /opt/pt/bin/libpng12.so.0.50.0 /opt/pt/bin/libpng12.so.0 
+		sudo ln -sf /opt/pt/bin/libpng12.so.0.50.0 /opt/pt/bin/libpng12.so.0
+
+		sudo rm -rf "$dir_libs" 1> /dev/null 2>&1
+ 
 	}
 		
 }
@@ -195,6 +232,8 @@ function _inst_libs_bionic()
 	sudo dpkg-deb -x "$arq_libpng12_deb8_amd64" "$dir_libs"
 	sudo cp -vu "${dir_libs}"/lib/x86_64-linux-gnu/libpng12.so.0.50.0 /opt/pt/bin/libpng12.so.0.50.0
 	sudo ln -sf /opt/pt/bin/libpng12.so.0.50.0 /opt/pt/bin/libpng12.so.0  
+	sudo rm -rf "$dir_libs" 1> /dev/null 2>&1
+
 }
 
 
@@ -213,14 +252,18 @@ function _corrigir_arquivos()
 [[ -f '/opt/pt/bin/Cisco-PacketTracer.desktop' ]] && {
 	sudo cp -u '/opt/pt/bin/Cisco-PacketTracer.desktop' '/usr/share/applications/' 
 	[[ -f '/usr/share/applications/pt7.desktop' ]] && sudo rm '/usr/share/applications/pt7.desktop'
-} 
+}
+ 
 } # Fim de corrigir arquivos
 
 
-#-----------------------------------[ Dependências ]----------------------#
-function _inst_dependencias() 
+
+
+#----------------------------- Execução --------------------#
+function cisco_packettracer() 
 {
 
+# Instalar dependências antes de instalar o programa.
 echo 'Instalando: gdebi aptitude multiarch-support qtmultimedia5-dev libqt5script5 libqt5scripttools5'
 echo ' '
 
@@ -229,16 +272,11 @@ echo ' '
 sudo apt update
 sudo apt install --yes gdebi aptitude multiarch-support qtmultimedia5-dev libqt5script5 libqt5scripttools5
 
-	[[ -d "$dir_tmp_pt" ]] && sudo rm -rf "$dir_tmp_pt"
-} 
-
-# Instalar dependências antes de instalar o programa.
-(_inst_dependencias) 
 
 # Executar a função de instalação se packettracer não estiver nas condições abaixo.
 if [[ ! -x $(which packettracer) ]] || [[ ! -x /opt/pt/bin/PacketTracer7 ]]; then
 
-instalar_agora=$(msgs_zenity "--list" "Instalar" "Selecione" "650" "250" "Instalação" "Instalar Sair")
+	instalar_agora=$(msgs_zenity "--list" "Instalar Cisco Packettracer" "Selecione" "650" "250" "Instalação" "Instalar Sair")
 
 	if [[ "$instalar_agora" == "Instalar" ]]; then
 		(_inst_ciscopkt) # Debian/Ubuntu.
@@ -247,6 +285,7 @@ instalar_agora=$(msgs_zenity "--list" "Instalar" "Selecione" "650" "250" "Instal
 	fi
 
 fi
+
 
 # Checar se local de instalação é '/opt/pt/bin'
 [[ -x /opt/pt/bin/PacketTracer7 ]] || { echo 'Cisco-PacketTracer não instalado em: [/opt/pt] saindo...'; exit 1; }
@@ -260,4 +299,9 @@ fi
 
 msgs_zenity "--info" "Reiniciar" "Reinicie seu computador para aplicar alterações" "550" "150"
 
-sudo -K
+}
+
+sudo -K #-------------------> Inicio
+(cisco_packettracer) #------> Meio # Tudo e feito apartir desta função
+sudo -K #-------------------> Fim
+
